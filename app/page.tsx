aae89488427567e57001161ce6d8cad7ee9b8e1b
Rahul Sharma <rs3296472t@gmail.com>
2025-03-25 13:07:24 +0530

    ssds

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
   const router=useRouter()
   return (
     <div>page
-      <Button onClick={()=>{router.push('/2feee7b7-629e-43c3-901c-8fa507a135f1&seller')}}>Bot</Button>
+      <Button onClick={()=>{router.push('bot/2feee7b7-629e-43c3-901c-8fa507a135f1&seller')}}>Bot</Button>
     </div>
   )
 }
