18a977148743f7ef25b93338186516bcd09c4182
Rahul Sharma <rs3296472t@gmail.com>
2025-03-25 13:02:58 +0530

    router

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+"use client"
+import {useRouter} from 'next/navigation'
 import React from 'react'
+import { Button } from './components/ui/button'
 
 const page = () => {
+  const router=useRouter()
   return (
-    <div>page</div>
+    <div>page
+      <Button onClick={()=>{router.push('/2feee7b7-629e-43c3-901c-8fa507a135f1&seller')}}></Button>
+    </div>
   )
 }
 
